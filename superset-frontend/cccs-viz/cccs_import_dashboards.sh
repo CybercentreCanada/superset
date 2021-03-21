cc372b2434df116f341e40f0078f7fcb31e25c67
cccs-jc <cccs-jc@cyber.gc.ca>
2021-03-21 13:07:36 +0000

    export/import cccs telemetry demo

---
+++ superset-frontend/cccs-viz/cccs_import_dashboards.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DASHBOARD_FILE=telemetry_dashboard.yaml
+
+echo "Copying dashboard file into docker container"
+docker cp $DASHBOARD_FILE superset_app:/app/$DASHBOARD_FILE
+
+echo "Imporing cccs dashboards"
+docker exec -it superset_app superset import-dashboards --path /app/$DASHBOARD_FILE
+
