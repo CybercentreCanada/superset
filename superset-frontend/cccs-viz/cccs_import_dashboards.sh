80f55150ba8f8515ee3f45c24ac83c2328fd0471
cccs-jc <cccs-jc@cyber.gc.ca>
2021-03-24 12:35:32 +0000

    code review fix

--- superset-frontend/cccs-viz/cccs_import_dashboards.sh
+++ superset-frontend/cccs-viz/cccs_import_dashboards.sh
@@ -5,6 +5,6 @@ DASHBOARD_FILE=telemetry_dashboard.yaml
 echo "Copying dashboard file into docker container"
 docker cp $DASHBOARD_FILE superset_app:/app/$DASHBOARD_FILE
 
-echo "Imporing cccs dashboards"
+echo "Importing cccs dashboards"
 docker exec -it superset_app superset import-dashboards --path /app/$DASHBOARD_FILE
 
