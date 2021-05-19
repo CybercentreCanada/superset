e15dd2be2f2b99cfe59fe8209dc28931b29871a8
cccs-mmrouet <mmrouet@mmrouet-u-dev-vm.53wq0amrcsbuxnt44apgcucn4h.ux.internal.cloudapp.net>
2021-05-19 17:56:46 +0000

    CLDN-750: initial commit. Boiler plating

---
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-at-a-glance/src/Styles.tsx
@@ -0,0 +1,32 @@
+
+// import { makeStyles } from '@material-ui/core/styles';
+
+// const useStyles = makeStyles(theme => ({
+//     datum: {
+//       border: '1px solid',
+//       borderColor: theme.palette.type === 'dark' ? theme.palette.grey[800] : theme.palette.grey[300],
+//       color: theme.palette.text.secondary,
+//       marginBottom: theme.spacing(1),
+//       padding: theme.spacing(1)
+//     },
+//     datumLink: {
+//       marginTop: theme.spacing(1)
+//     },
+//     sectionTitle: {
+//       display: 'flex',
+//       alignItems: 'center',
+//       padding: theme.spacing(1)
+//     },
+//     urlList: {
+//       hight: '100%',
+//       width: '100%',
+//       maxWidth: 360,
+//       overflow: 'auto',
+//       maxHeight: 100,
+//       float: 'left',
+//     },
+//     urlListItem: {
+//       backgroundColor: 'inherit',
+//       padding: 0,
+//     },
+//   }));
