22be369b5920e2bbe449d3614978700e9b86cc5a
harman-sehra <83774506+harman-sehra@users.noreply.github.com>
2021-07-08 10:17:42 -0400

    [CLDN-743] Adressing last column name being cutoff in explore (#32)

--- superset-frontend/src/explore/components/DatasourcePanel/index.tsx
+++ superset-frontend/src/explore/components/DatasourcePanel/index.tsx
@@ -67,6 +67,7 @@ const DatasourceContainer = styled.div`
   .field-selections {
     padding: ${({ theme }) => `0 0 ${4 * theme.gridUnit}px`};
     overflow: auto;
+    margin-bottom: 20px;
   }
   .field-length {
     margin-bottom: ${({ theme }) => theme.gridUnit * 2}px;
@@ -218,7 +219,7 @@ export default function DataSourcePanel({
 
   const mainBody = (
     <>
-      <div className="field-selections" style={{overflowY: 'scroll', height: '88%'}}>
+      <div className="field-selections">
         <input
           type="text"
           onChange={evt => {
