2b5a367d8a5ae10f945fabae4d7cb7fc8a6cea1e
cccs-mmrouet <62021113+cccs-mmrouet@users.noreply.github.com>
2021-07-12 09:24:11 -0400

    CLDN-750 : Fixed empty query error and default viz for control panel.

---
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-at-a-glance/src/styles.js
@@ -0,0 +1,44 @@
+const Datum = {
+    border: '1px solid',
+    borderColor:'#20A7C9',
+    fontSize: "14px",
+    marginBottom: 2,
+    padding: 2
+  }
+
+  const DatumLink = {
+    marginTop: 1
+  }
+
+  const SectionTitle = {
+    display: 'flex',
+    alignItems: 'center',
+    fontSize: "22px",
+    padding: 1,
+    spacing: 1
+  }
+
+  const UrlList = {
+    hight: '100%',
+    width: '100%',
+    maxWidth: 360,
+    overflow: 'auto',
+    maxHeight: 100,
+    float: 'left',
+  }
+
+  const UrlListItem = {
+    backgroundColor: 'inherit',
+    padding: 0,
+  }
+
+
+  const styles = {
+    Datum : Datum,
+    DatumLink : DatumLink,
+    SectionTitle : SectionTitle,
+    UrlList : UrlList,
+    UrlListItem: UrlListItem, 
+  }
+
+  export default styles;
