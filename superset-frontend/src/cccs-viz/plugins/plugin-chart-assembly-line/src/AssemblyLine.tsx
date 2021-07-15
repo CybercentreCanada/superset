95068b063677d4b6928ce81d79509cb932575bcd
bender519 <Daniel.Fanick@cyber.gc.ca>
2021-07-15 13:29:59 -0400

    'CLDN-781': tests search in Assembly Line of a given IP address in AL visualization for superset

--- superset-frontend/src/cccs-viz/plugins/plugin-chart-assembly-line/src/AssemblyLine.tsx
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-assembly-line/src/AssemblyLine.tsx
@@ -1,17 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import { styled } from '@superset-ui/core';
+import React, { useState, useEffect } from 'react';
+import { AssemblyLineProps } from './types';
+import { default as dummy } from './plugin/dummy';
+import GenericComponent from './plugin/GenericComponent';
 
-type AssemblyLineProps = {
-    width: number;
-    height: number;
-    
-}
+const useDataApi = (initialIP: string, initialData: number | undefined): number | undefined => {
+    const [data, setData] = useState(initialData);
+    const [ip, setIP] = useState(initialIP);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
 
-export default function AssemblyLine({
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsError(false);
+            setIsLoading(true);
 
-}) {
+            try {
+                const result = await dummy();
+                
+                if (typeof result === "string") {
+                    const jsonData = JSON.parse(JSON.parse(result));
+                    setData(jsonData["api_response"]["items"].length);
+                }
+                else {
+                    setData(undefined);
+                }
+
+            } catch (error) {
+                setIsError(true);
+            }
+
+            setIsLoading(false);
+        };
+
+        fetchData();
+    }, [ip]);
+
+    return data;
+};
+
+export default function AssemblyLine(props: AssemblyLineProps) {
+
+    let totalTimesSeen = useDataApi(
+        props.ipAddress,
+        undefined,
+    );
+
+    let finalTotalTimesSeen: number = totalTimesSeen ? totalTimesSeen : 0;
+
     return (
-        <>
-        </>
+        <GenericComponent totalTimesSeen={finalTotalTimesSeen} ipAddress={props.ipAddress} />
     );
 }
