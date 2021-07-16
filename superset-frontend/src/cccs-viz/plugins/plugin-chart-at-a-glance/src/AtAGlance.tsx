786a94a6af66fd414977a4329c44b0497e043bde
cccs-mmrouet <62021113+cccs-mmrouet@users.noreply.github.com>
2021-07-16 09:55:07 -0400

    CLDN-750: Adding associated hostname query

--- superset-frontend/src/cccs-viz/plugins/plugin-chart-at-a-glance/src/AtAGlance.tsx
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-at-a-glance/src/AtAGlance.tsx
@@ -49,23 +49,72 @@ import { Row, Col, Grid} from 'react-bootstrap/';
 import styles from './styles';
 import IPAddressUtil from './IpAddressUtil';
 
+/**
+*   getPayloadField:
+*     description: Returns the value for a given field.
+*     parameters:
+*       - name: field
+*       - type: string
+*       - required: true
+*       - description: name of the field you want to get the value of  
+*
+*       - name: payload
+*       - type: any
+*       - required: true
+*       - description: data object containing the response from the server.  
+*     returns:
+*       value:
+*         description: Returns the value of the given field if found.
+*/
 const getPayloadField = (field: string, payload: any) => {
   try{
-    const value = payload[field];
-    if (value !== undefined) {
+    if (payload != undefined) {
+      const value = payload[field];
+      if (value !== null) {
         return value;
+      }
+      else{
+        return "Unknown";
+      }
     }
-  } catch (e)
+  }catch (e)
   {
     console.log(e);
     return "Something went wrong";
   }
-  return "Unknown";
 };
 
+function getHostnames (payload: any) { 
+  const uniqueSet = new Set(payload.map(a => a.rrname));
+  const result = [...uniqueSet];
+  console.log("payload: "  + JSON.stringify(payload));
+  console.log("hostnames: "  + JSON.stringify(result));
+  return result;
+  // if (props.isFarsightLoading && !props.isFarsightInit) { 
+  //   return "Loading ...";
+  // }
+  // return getPayloadField("rrdata", props.farsightData).map((hostname) => console.log);
+}
+
+/**
+*   buildGeoFormData:
+*     description: builds the formData object that will be passed to the server in the request.
+*     parameters:
+*       - name: currentFormData
+*       - type: QueryFormData
+*       - required: true
+*       - description: current formdata object that needs to be updated  
+*
+*       - name: ip
+*       - type: string
+*       - required: true
+*       - description: ip that needs to be added to the current form data.  
+*     returns:
+*       value:
+*         description: Returns the appropriately filled form data.
+*/
 const buildGeoFormData = (currentFormData: QueryFormData, ip: string) =>{
   const numericalIP = IPAddressUtil.textToNumericFormatV4(ip);
-  console.log("buildGeoForm - numeric IP:" + numericalIP);
 
   // Filter that will be applied to all the queries for each datasrouce. Use this statement when ip_string is available in datasource.
   // const filter : AdhocFilter = {expressionType: 'SIMPLE', clause: 'WHERE', subject: 'ip_string', operator: 'IN', comparator: [ipString] };
@@ -74,6 +123,7 @@ const buildGeoFormData = (currentFormData: QueryFormData, ip: string) =>{
      
   const startIPFilter : AdhocFilter = {expressionType: 'SIMPLE', clause: 'WHERE', subject: 'start_ip_int', operator: '<=', comparator: numericalIP.toString(10) };
   const endIPFilter : AdhocFilter = {expressionType: 'SIMPLE', clause: 'WHERE', subject: 'end_ip_int', operator: '>=', comparator: numericalIP.toString(10)};
+ 
   // Setting up newStarGeo query:
   const newStarGeoFormData : QueryFormData = JSON.parse(JSON.stringify(currentFormData));
   newStarGeoFormData.adhoc_filters = [startIPFilter, endIPFilter];
@@ -83,8 +133,43 @@ const buildGeoFormData = (currentFormData: QueryFormData, ip: string) =>{
   return newStarGeoFormData;
 }
 
+/**
+*   buildFarsightFormData:
+*     description: builds the formData object that will be passed to the server in the request.
+*     parameters:
+*       - name: currentFormData
+*       - type: QueryFormData
+*       - required: true
+*       - description: current formdata object that needs to be updated  
+*
+*       - name: ip
+*       - type: string
+*       - required: true
+*       - description: ip that needs to be added to the current form data.  
+*     returns:
+*       value:
+*         description: Returns the appropriately filled form data.
+*/
+const buildFarsightFormData = (currentFormData: QueryFormData, ip: string) =>{
+   const farsightFormData : QueryFormData = JSON.parse(JSON.stringify(currentFormData));
+  
+  // Setting up the filters
+  const IPFilter : AdhocFilter = {expressionType: 'SIMPLE', clause: 'WHERE', subject: 'rdata', operator: 'IN', comparator: [ip] };
+
+  farsightFormData.adhoc_filters  = [IPFilter];
+  farsightFormData.metrics = undefined;
+  farsightFormData.columns = ["rrname"];
+  farsightFormData.datasource = "58__table";
+  farsightFormData.limit = 10;
+  return farsightFormData;
+}
+
+const isPayloadUndefined = (payload : any) =>{
+  return payload == null;
+}
+
 const useDataApi = (formData: QueryFormData) => {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]); 
   const [isInit, setIsinit] = useState(true)
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
@@ -107,7 +192,7 @@ const useDataApi = (formData: QueryFormData) => {
         setIsLoading(true);
         setIsError(false);
         const response = await asyncChartDataRequest;
-        const newData = response.result[0].data[0];
+        const newData = response.result[0].data;
         setIsLoading(false);
         setData(newData);
       } catch (error) {
@@ -119,31 +204,24 @@ const useDataApi = (formData: QueryFormData) => {
       setIsLoading(false);
     };
     fetchLookupDetails();
-    console.log("In useDataApi end - data: " + JSON.stringify(data));
   }, [formData]);
   return [{ data, isLoading, isError, isInit }] as const;
 };
 
 function AtAGlanceCore ( initialFormData: QueryFormData) {
   console.log("In At A Glance");
-  const [ipString, setIpString] = useState('34.214.200.224');
+  const [ipString, setIpString] = useState('3.251.148.10');
   const [formData, setFormData] = useState(initialFormData);
   const [newStarGeoFormData, setNewStarGeoFormData] = useState(buildGeoFormData(formData, ipString));
-
+  const [farsightFormData, setFarsightFormData] = useState(buildFarsightFormData(formData, ipString));
  
-  // Setting up farsight query:
-  // const farsightFormData : QueryFormData = JSON.parse(JSON.stringify(formData));
-  // const rdataFilter =  AdhocFilter = {expressionType: 'SQL', clause: 'WHERE', sqlExpression:"where start_ip_int = 217384448 OR end_ip_int = 217384448"};
-  // farsightFormData.metrics = undefined;
-  // farsightFormData.columns = ["rrname"];
-  // farsightFormData.datasource = "58__table";
-  // //farsightFormData.adhoc_filters  = [filter];
-
   // Query executions:
-  console.log("newStarGeoForm: " + JSON.stringify(newStarGeoFormData));
   const [{ data : geoData, isLoading: isGeoLoading, isInit : isGeoInit, isError: isGeoError}] = useDataApi(newStarGeoFormData);
-  console.log("geoData: " + JSON.stringify(geoData));
-  //const [{ data: farsightData, isLoading: isFarsightLoading, isError: isFarsightError }] = useDataApi(farsightFormData);
+  //console.log("geoData: " + JSON.stringify(geoData));
+
+  const [{ data: farsightData, isLoading: isFarsightLoading, isError: isFarsightError }] = useDataApi(farsightFormData);
+  //console.log("farsightData: "  + JSON.stringify(farsightData));
+
 
   const [inputIp, setInputIp] = useState(ipString);
   const submit = event => {
@@ -153,9 +231,29 @@ function AtAGlanceCore ( initialFormData: QueryFormData) {
       event.preventDefault();
       console.log("event value: " + inputIp);
       setNewStarGeoFormData(buildGeoFormData(formData, inputIp));
+      setFarsightFormData(buildFarsightFormData(formData, inputIp));
       setIpString(inputIp);
       console.log("ipstring in submit event" + ipString);
   };
+
+  if (isPayloadUndefined(geoData) && !isGeoLoading)
+    return (
+    <>  
+    <div style={styles.SectionTitle}>
+      <RiGlobalFill /> <span> At a Glance </span>
+    </div>
+    <div style={styles.Datum}>
+      <form autoComplete="off" onSubmit={submit}>
+          <label>
+            IP:
+            <input type="text" value={inputIp} onChange={e => {setInputIp(e.target.value); console.log( "e value" + e.target.value )} }/>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+    </div>
+      <h3>No data found for this identifier </h3>
+    </>
+    );
 
   return (
     <>
@@ -174,25 +272,25 @@ function AtAGlanceCore ( initialFormData: QueryFormData) {
     <div style={styles.Datum}>
       <Grid fluid >
         <Row >
-          <Col> ASN: {isGeoLoading && !isGeoInit ? "Loading ..." : getPayloadField("asn", geoData)} </Col>
+          <Col> ASN: {isGeoLoading && !isGeoInit ? "Loading ..." : getPayloadField("asn", geoData[0])} </Col>
         </Row>
         <Row >
-          <Col> CARRIER: {isGeoLoading && !isGeoInit ? "Loading ..." : getPayloadField("carrier", geoData)} </Col>
+          <Col> CARRIER: {isGeoLoading && !isGeoInit ? "Loading ..." : getPayloadField("carrier", geoData[0])} </Col>
         </Row>
         <Row >
-          <Col> CONNECTION TYPE: {isGeoLoading && !isGeoInit ? "Loading ..." :  getPayloadField("connection_type", geoData)} </Col>
+          <Col> CONNECTION TYPE: {isGeoLoading && !isGeoInit ? "Loading ..." :  getPayloadField("connection_type", geoData[0])} </Col>
         </Row>
         <Row >
-          <Col> ORGANIZATION: {isGeoLoading && !isGeoInit ? "Loading ..." : getPayloadField("organization", geoData)} </Col>
+          <Col> ORGANIZATION: {isGeoLoading && !isGeoInit ? "Loading ..." : getPayloadField("organization", geoData[0])} </Col>
         </Row>
         <Row >
-          <Col> CITY: {isGeoLoading && !isGeoInit ? "Loading ..." : getPayloadField("city", geoData)} </Col>
+          <Col> CITY: {isGeoLoading && !isGeoInit ? "Loading ..." : getPayloadField("city", geoData[0])} </Col>
         </Row>
         <Row >
-          <Col> COUNTRY: {isGeoLoading && !isGeoInit ? "Loading ..." : getPayloadField("country", geoData)} </Col>
+          <Col> COUNTRY: {isGeoLoading && !isGeoInit ? "Loading ..." : getPayloadField("country", geoData[0])} </Col>
         </Row>
         <Row >
-          <Col> DECIMAL: {isGeoLoading && !isGeoInit ? "Loading ..." : getPayloadField("decimal", geoData)} </Col>
+          <Col> DECIMAL: {isGeoLoading && !isGeoInit ? "Loading ..." : getPayloadField("decimal", geoData[0])} </Col>
         </Row>
         <Row >
           <Col> VIRUSTOTAL COUNT: {isGeoLoading && !isGeoInit ? "Loading ..." : "Not yet available"} </Col>
@@ -201,7 +299,13 @@ function AtAGlanceCore ( initialFormData: QueryFormData) {
           <Col> ASSOCIATED HOSTNAMES: {isGeoLoading && !isGeoInit ? "Loading ..." : "Not yet available"} </Col>
         </Row>
       </Grid>
-    </div>   
+    </div>
+    <div>
+      <h3>ASSOCIATE HOSTNAMES</h3>
+      <ul> 
+        {isGeoLoading && !isGeoInit ? "Loading ..." :  getHostnames(farsightData).map((hostname) =>  <li>{hostname}</li>)  } 
+      </ul> 
+    </div>    
     </> 
   );
 };
