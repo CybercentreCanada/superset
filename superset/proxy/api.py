8b9209e63a819e101a6801cd4c7a78d2ac1b7487
cccs-RyanS <71385290+cccs-RyanS@users.noreply.github.com>
2021-11-01 12:12:27 -0400

    Adding ip alfred fliter

--- superset/proxy/api.py
+++ superset/proxy/api.py
@@ -24,7 +24,7 @@ class ProxyRestAPI(BaseSupersetModelRestApi):
     """
     datamodel = SQLAInterface(SqlaTable)
 
-    include_route_methods = {"get"}
+    include_route_methods = {"get", "get_ipstring"}
     resource_name = "proxy"
 
     openapi_spec_tag = "Proxy"
@@ -35,6 +35,77 @@ class ProxyRestAPI(BaseSupersetModelRestApi):
         log_to_statsd=False, # pylint: disable-arguments-renamed
     )
     def get(self, id : str, **kwargs: Any) -> Response:
+
+
+        alfred_client_id = os.getenv('ALFRED_CLIENT_ID') 
+        alfred_token = ''
+
+        client_id = os.getenv('SUPERSET_CLIENT_ID') 
+        tenant_id = os.getenv('SUPERSET_TENANT_ID') 
+        client_secret = os.getenv('SUPERSET_CLIENT_SECRET')  
+
+        url = 'https://login.microsoftonline.com/{0}/oauth2/v2.0/token'.format(tenant_id)
+        refresh_token = ""
+        
+        with open('refresh_token.pkl', 'rb') as f:
+            refresh_token = pickle.load(f)
+
+        if refresh_token:
+            refresh_params = {
+                "client_id": client_id,
+                "client_secret": urllib.parse.quote(client_secret),
+                "grant_type": "refresh_token",
+                "refresh_token": refresh_token
+            }
+
+            # URL encoding: client secret must be URL encoded, but grant_type and scope must not be encoded
+            refresh_data = '&'.join(["{}={}".format(k, v) for k, v in refresh_params.items()])
+            try:
+                refresh_resp = requests.post(url, data = refresh_data)
+            except Exception as e:
+                print("Error obtaining fresh access token: %s" % e)
+
+            refresh_resp_json = json.loads(refresh_resp.content.decode('utf8', 'replace'))
+            current_access_token = refresh_resp_json['access_token']
+
+            alfred_obo_params = {
+                "client_id": client_id,
+                "client_secret": client_secret,
+                "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
+                "assertion": current_access_token,
+                "requested_token_use": "on_behalf_of",
+                "scope": '{0}/Alfred.ALL'.format(alfred_client_id)
+            }
+
+            alfred_obo_data = '&'.join(["{}={}".format(k, v) for k, v in alfred_obo_params.items()])
+
+            try:
+                alfred_obo_resp = requests.post(url, data = alfred_obo_data)
+            except Exception as e:
+                print("Error obtaining on-behalf-of Fission token: %s" % e)
+
+            alfred_obo_resp_json = json.loads(alfred_obo_resp.content.decode('utf8', 'replace'))
+            alfred_token = alfred_obo_resp_json["access_token"]
+
+            headers = CaseInsensitiveDict()
+            headers["Accept"] = "application/json"
+            headers["Authorization"] = f"Bearer { alfred_token }"
+            url = "https://alfred-tst.u.chimera.azure.cyber.gc.ca:9488/rest/search/cypher?expression=MATCH%20(email:EMAIL_ADDRESS)%20WHERE%20email.value%20in%20[%22" + id + "%22]%20return%20email.value,%20email.maliciousness,%20email.uri"
+            alfred_resp = ""
+            try:
+                alfred_resp = requests.get(url, headers=headers)
+            except Exception as e:
+                print("Error obtaining on-behalf-of Fission token: %s" % e)
+            refresh_resp_json = json.loads(alfred_resp.content.decode('utf8', 'replace'))
+            print(refresh_resp_json)
+        return self.response(200,payload=refresh_resp_json)
+
+    @expose("/alfred/ip_string/<string:id>", methods=["GET"])
+    @event_logger.log_this_with_context(
+        action=lambda self, *args, **kwargs: f"{self.__class__.__name__}.get",
+        log_to_statsd=False, # pylint: disable-arguments-renamed
+    )
+    def get_ipstring(self, id : str, **kwargs: Any) -> Response:
 
 
         alfred_client_id = os.getenv('ALFRED_CLIENT_ID') 
@@ -90,7 +161,7 @@ class ProxyRestAPI(BaseSupersetModelRestApi):
             headers = CaseInsensitiveDict()
             headers["Accept"] = "application/json"
             headers["Authorization"] = f"Bearer { alfred_token }"
-            url = "https://alfred-tst.u.chimera.azure.cyber.gc.ca:9488/rest/search/cypher?expression=MATCH%20(email:EMAIL_ADDRESS)%20WHERE%20email.value%20in%20[%22" + id + "%22]%20return%20email.value,%20email.maliciousness,%20email.uri"
+            url = "https://alfred-tst.u.chimera.azure.cyber.gc.ca:9488/rest/search/cypher?expression=MATCH%20(ip%3AIP_ADDRESS)%20WHERE%20ip.value%20IN%20%5B%22" + id + "%22%5D%20RETURN%20ip.value%2C%20ip.maliciousness%2C%20ip.creation_date%2C%20ip.created_by%2C%20ip.uri%2C%20ip.report_uri"
             alfred_resp = ""
             try:
                 alfred_resp = requests.get(url, headers=headers)
