83946c723740f438903bd395e677d52c2c529cac
Reese <10563996+reesercollins@users.noreply.github.com>
2022-08-09 10:54:47 -0400

    Add path to url (#193)

--- superset-frontend/src/views/CRUD/data/dataset/DatasetList.tsx
+++ superset-frontend/src/views/CRUD/data/dataset/DatasetList.tsx
@@ -322,7 +322,7 @@ const DatasetList: FunctionComponent<DatasetListProps> = ({
             const parsedExtra = JSON.parse(extra);
             if (parsedExtra?.urn) {
               return (
-                <a href={datahubUrl + parsedExtra?.urn}>
+                <a href={`${datahubUrl}dataset/${parsedExtra?.urn}`}>
                   <Icons.Datahub viewBox="0 0 180 180" />
                 </a>
               );
