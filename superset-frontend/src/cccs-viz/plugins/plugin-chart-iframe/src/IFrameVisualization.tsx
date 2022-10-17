02763ef9941f7ad3a9ecd8281abdb4187c689d1e
cccs-RyanS <71385290+cccs-RyanS@users.noreply.github.com>
2022-10-17 15:40:54 -0400

    Adding generic filter extraction

--- superset-frontend/src/cccs-viz/plugins/plugin-chart-iframe/src/IFrameVisualization.tsx
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-iframe/src/IFrameVisualization.tsx
@@ -6,6 +6,6 @@ export default function IFrameVisualization(props: IFrameVisualizationProps) {
   const { url, url_parameter_value, parameter_name } = props
   console.log(`${url}?${parameter_name}=${url_parameter_value}`)
   return (
-    <iframe src={`${url}?${parameter_name}=${url_parameter_value}`} style={{ position: 'absolute', left:0, top: '50px', width:'95%', height:'100%' }}></iframe>
+    <iframe src={`${url}?${parameter_name}${url_parameter_value}`} style={{ position: 'absolute', left:0, top: '50px', width:'95%', height:'100%' }}></iframe>
   );
 }
