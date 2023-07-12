863c16d11849c0a6edf2aee091df2a546cbbef71
cccs-rc <62034438+cccs-rc@users.noreply.github.com>
2023-07-12 10:23:19 -0400

    Feature/cldn 2170 (#353)
    * CLDN-2170 Encoding parameter prefix and parameter value
    
    * CLDN-2170 Refactoring to ensure 'undefined' value doesn't appear for paremeter_prefix if = is not included in parameter_name

--- superset-frontend/src/cccs-viz/plugins/plugin-chart-iframe/src/IFrameVisualization.tsx
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-iframe/src/IFrameVisualization.tsx
@@ -10,9 +10,11 @@ export default function IFrameVisualization(props: IFrameVisualizationProps) {
     errorMessage,
   } = props;
 
-  const parserdUrlParameterName = parameter_name.includes('=')
-    ? parameter_name
-    : `${parameter_name}=${parameter_prefix}`;
+  // We encode the parameter value (including its prefix)
+  // so the Fission proxy doesn't break on parameters that contain, for example, URLs.
+  const encodedUrl = `${url}?${parameter_name}=${
+    parameter_prefix ? encodeURIComponent(parameter_prefix) : ''
+  }${encodeURIComponent(url_parameter_value)}`;
 
   return (
     <>
@@ -20,7 +22,7 @@ export default function IFrameVisualization(props: IFrameVisualizationProps) {
         <>{errorMessage}</>
       ) : (
         <iframe
-          src={`${url}?${parserdUrlParameterName}${url_parameter_value}`}
+          src={encodedUrl}
           style={{
             position: 'absolute',
             left: 0,
