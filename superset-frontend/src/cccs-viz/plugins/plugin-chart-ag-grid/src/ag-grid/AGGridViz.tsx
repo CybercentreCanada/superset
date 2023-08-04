8223c2c98738cb5709770403eeafd0030688bb00
cccs-RyanK <ryan.kane@cyber.gc.ca>
2023-08-04 19:39:32 +0000

    added more options to the customize tab

--- superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/ag-grid/AGGridViz.tsx
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/ag-grid/AGGridViz.tsx
@@ -1,14 +1,11 @@
-'use strict';
 import React, {
-    ChangeEvent,
-    useCallback,
-    useEffect,
-    useMemo,
-    useRef,
-    useState,
-  } from 'react';
-
-import { AGGridVizProps } from '../types'
+  ChangeEvent,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 
 import { AgGridReact } from 'ag-grid-react';
 import { AgGridReact as AgGridReactType } from 'ag-grid-react';
@@ -22,60 +19,163 @@ import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 import { ModuleRegistry } from '@ag-grid-community/core';
-import { ChartRangeSelectionChanged, RangeSelectionChangedEvent } from 'ag-grid-community';
+import {
+  ChartRangeSelectionChanged,
+  RangeSelectionChangedEvent,
+} from 'ag-grid-community';
+import { AGGridVizTransformedProps } from '../types';
+import { PAGE_SIZE_OPTIONS } from '../cccs-grid/plugin/controlPanel';
 // Register the required feature modules with the Grid
-ModuleRegistry.registerModules([ClientSideRowModelModule, RangeSelectionModule, RowGroupingModule, RichSelectModule]);
+ModuleRegistry.registerModules([
+  ClientSideRowModelModule,
+  RangeSelectionModule,
+  RowGroupingModule,
+  RichSelectModule,
+]);
 
-export default function AGGridViz({columnDefs, rowData, formData, height, width }: AGGridVizProps) {
-    
-    const [columnDefsStateful, setColumnDefsStateful] = useState(columnDefs)
-    
-    const [rowDataStateful, setrowDataStateful] = useState(rowData)
+export default function AGGridViz({
+  columnDefs,
+  rowData,
+  formData,
+  height,
+  width,
+  includeSearch,
+  pageLength = 0,
+  enableGrouping,
+}: AGGridVizTransformedProps) {
+  const [columnDefsStateful, setColumnDefsStateful] = useState(columnDefs);
+  const [searchValue, setSearchValue] = useState('');
+  const [pageSize, setPageSize] = useState<number>(pageLength);
+  const [rowDataStateful, setrowDataStateful] = useState(rowData);
 
-    useEffect(() => {
-        setColumnDefsStateful(columnDefs);
-    }, columnDefs);
+  useEffect(() => {
+    setColumnDefsStateful(columnDefs);
+  }, [columnDefs]);
 
-    useEffect(() => {
-        setrowDataStateful(rowData);
-    }, rowData);
+  useEffect(() => {
+    setrowDataStateful(rowData);
+  }, [rowData]);
 
-    const defaultColDef = useMemo(() => ({
-        resizable: true,
-        sortable: true
-    }), []);
+  const defaultColDef = useMemo(
+    () => ({
+      resizable: true,
+      sortable: true,
+    }),
+    [],
+  );
 
-    const gridRef = useRef<AgGridReactType>(null);
+  const gridRef = useRef<AgGridReactType>(null);
 
-    const onRangeSelectionChanged = (event: RangeSelectionChangedEvent) => {
-       if (event.finished) {
-        console.log("Event Finished")
-       }
-       else if (event.started) {
-        console.log("Event Starated")
-       }
-       else {
-        console.log("Event runnning")
-       }
+  const onRangeSelectionChanged = (event: RangeSelectionChangedEvent) => {
+    if (event.finished) {
+      console.log('Event Finished');
+    } else if (event.started) {
+      console.log('Event Starated');
+    } else {
+      console.log('Event runnning');
     }
+  };
 
-    return (
-        <div
-            style={{ width: width, height: height, display: 'flex', flexFlow: 'column' }}
-            >
-            <AgGridReact
-                ref={gridRef}
-                className="ag-theme-alpine" 
-                columnDefs={columnDefsStateful}
-                defaultColDef={defaultColDef}
-                enableRangeSelection={true}
-                rowData={rowDataStateful}
-                //rowSelection="multiple"
-                onRangeSelectionChanged={onRangeSelectionChanged}
-                suppressRowClickSelection={true}
-                modules={[ClientSideRowModelModule, RangeSelectionModule, RowGroupingModule, RichSelectModule]}
-            />
+  const updatePageSize = (newSize: number) => {
+    gridRef.current?.api?.paginationSetPageSize(newSize);
+    setPageSize(newSize <= 0 ? 0 : newSize);
+  };
+
+  useEffect(() => {
+    updatePageSize(pageLength);
+  }, [pageLength]);
+
+  const setSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    e.preventDefault();
+    setSearchValue(target.value);
+  };
+
+  useEffect(() => {
+    if (!includeSearch) {
+      setSearchValue('');
+    }
+  }, [includeSearch]);
+
+  return (
+    <div
+      style={{
+        width,
+        height,
+        display: 'flex',
+        flexFlow: 'column',
+      }}
+    >
+      <div
+        className="form-inline"
+        style={{ flex: '0 1 auto', paddingBottom: '0.5em' }}
+      >
+        <div className="row">
+          <div className="col-sm-6">
+            {pageLength > 0 && (
+              <span className="dt-select-page-size form-inline">
+                Show{' '}
+                <select
+                  className="form-control input-sm"
+                  value={pageSize}
+                  onBlur={() => {}}
+                  onChange={e => {
+                    updatePageSize(
+                      Number((e.target as HTMLSelectElement).value),
+                    );
+                  }}
+                >
+                  {PAGE_SIZE_OPTIONS.map(option => {
+                    const [size, text] = Array.isArray(option)
+                      ? option
+                      : [option, option];
+                    return (
+                      <option key={size} value={size}>
+                        {text}
+                      </option>
+                    );
+                  })}
+                </select>{' '}
+                entries
+              </span>
+            )}
+          </div>
+          <div className="col-sm-6">
+            {includeSearch ? (
+              <span className="float-right">
+                Search{' '}
+                <input
+                  className="form-control input-sm"
+                  placeholder={`${rowData.length} records...`}
+                  value={searchValue}
+                  onChange={setSearch}
+                />
+              </span>
+            ) : null}
+          </div>
         </div>
-    );
+      </div>
+      <AgGridReact
+        ref={gridRef}
+        className="ag-theme-alpine"
+        columnDefs={columnDefsStateful}
+        defaultColDef={defaultColDef}
+        enableRangeSelection
+        rowData={rowDataStateful}
+        onRangeSelectionChanged={onRangeSelectionChanged}
+        suppressRowClickSelection
+        modules={[
+          ClientSideRowModelModule,
+          RangeSelectionModule,
+          RowGroupingModule,
+          RichSelectModule,
+        ]}
+        paginationPageSize={pageSize}
+        pagination={pageSize > 0}
+        cacheQuickFilter
+        quickFilterText={searchValue}
+        rowGroupPanelShow={enableGrouping ? 'always' : 'never'}
+      />
+    </div>
+  );
 }
-
--- superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/cccs-grid/plugin/transformProps.ts
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/cccs-grid/plugin/transformProps.ts
@@ -10,28 +10,71 @@ import {
   TimeseriesDataRecord,
 } from '@superset-ui/core';
 
-import { CccsGridChartProps } from '../../types';
+import { CccsTableChartProps, CccsTableFormData } from '../../types';
+import ExpandAllValueRenderer from '../../ExpandAllValueRenderer';
 
 const calcColumnDefs = (columns: QueryFormColumn[]) =>
-  columns.map(col => ({ field: col, Blah: 'Yeeeea' }));
+  columns.map(col => ({ field: col }));
 
-export default function transformProps(chartProps: CccsGridChartProps) {
+export default function transformProps(chartProps: CccsTableChartProps) {
+  const { width, height, formData, queriesData } = chartProps;
   const {
-    datasource,
-    hooks,
-    width,
-    height,
-    formData,
-    queriesData,
-    emitCrossFilters,
-  } = chartProps;
-
-  const columnDefs =
+    includeSearch,
+    pageLength,
+    defaultGroupBy,
+    enableRowNumbers,
+    enableGrouping,
+    enableJsonExpand,
+  }: CccsTableFormData = {
+    ...formData,
+  };
+  let columnDefs =
     formData.queryMode === QueryMode.raw
       ? calcColumnDefs(formData.allColumns || [])
       : calcColumnDefs(formData.groupby || []);
 
   const data = queriesData[0].data as TimeseriesDataRecord[];
+
+  if (enableRowNumbers) {
+    columnDefs.splice(0, 0, {
+      headerName: '#',
+      colId: 'rowNum',
+      pinned: 'left',
+      lockVisible: true,
+      valueGetter: (params: any) =>
+        params.node ? params.node.rowIndex + 1 : null,
+    } as any);
+  }
+
+  // If the flag is set to true, add a column which will contain
+  // a button to expand all JSON blobs in the row
+  if (enableJsonExpand) {
+    columnDefs.splice(1, 0, {
+      colId: 'jsonExpand',
+      pinned: 'left',
+      cellRenderer: ExpandAllValueRenderer,
+      autoHeight: true,
+      minWidth: 105,
+      lockVisible: true,
+    } as any);
+  } else if (enableGrouping) {
+    // enable row grouping
+    columnDefs = columnDefs.map(c => {
+      const enableRowGroup = true;
+      const rowGroupIndex = defaultGroupBy.findIndex(
+        (element: any) => element === c.field,
+      );
+      const rowGroup = rowGroupIndex >= 0;
+      const hide = rowGroup;
+      return {
+        ...c,
+        enableRowGroup,
+        rowGroup,
+        rowGroupIndex: rowGroupIndex === -1 ? null : rowGroupIndex,
+        hide,
+      };
+    });
+  }
 
   return {
     width,
@@ -39,5 +82,8 @@ export default function transformProps(chartProps: CccsGridChartProps) {
     formdata: chartProps.formData,
     rowData: data,
     columnDefs,
+    includeSearch,
+    pageLength,
+    enableGrouping,
   };
 }
