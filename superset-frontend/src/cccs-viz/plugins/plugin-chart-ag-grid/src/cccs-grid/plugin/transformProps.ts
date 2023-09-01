83069e4ef7b2b477dbd846aacc3d3700edbc3646
cccs-RyanK <ryan.kane@cyber.gc.ca>
2023-09-01 18:20:35 +0000

    increased row number width

--- superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/cccs-grid/plugin/transformProps.ts
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/cccs-grid/plugin/transformProps.ts
@@ -1,12 +1,22 @@
-import { Column, Metric, NumberFormats, QueryFormColumn, TimeseriesDataRecord, getNumberFormatter } from '@superset-ui/core';
+import {
+  Column,
+  Metric,
+  NumberFormats,
+  QueryFormColumn,
+  TimeseriesDataRecord,
+  getNumberFormatter,
+} from '@superset-ui/core';
 
 import { CccsTableChartProps, CccsTableFormData } from '../../types';
 import { ValueFormatterParams } from 'ag-grid-community';
 import ExpandAllValueRenderer from '../../types/ExpandAllValueRenderer';
 import { rendererMap } from '../../types/advancedDataTypes';
 
-const calcMetricColumnDefs = (metrics: any[], percent_metrics: any[], datasource_metrics: Metric[]) => {
-  
+const calcMetricColumnDefs = (
+  metrics: any[],
+  percent_metrics: any[],
+  datasource_metrics: Metric[],
+) => {
   const percentMetricValueFormatter = function (params: ValueFormatterParams) {
     return getNumberFormatter(NumberFormats.PERCENT_3_POINT).format(
       params.value,
@@ -22,47 +32,47 @@ const calcMetricColumnDefs = (metrics: any[], percent_metrics: any[], datasource
     return metricMap;
   }, metricVerboseNameMap);
 
-
   let columnDefs: any[] = [];
 
   if (metrics) {
-    const metricsColumnDefs = metrics
-      .map((metric: any) => {
-        const metricHeader = metricVerboseNameMap[metric]
-          ? metricVerboseNameMap[metric]
-          : metric;
-        return {
-          field: metric,
-          headerName: metricHeader,
-          sortable: true,
-          enableRowGroup: true,
-        };
-      });
+    const metricsColumnDefs = metrics.map((metric: any) => {
+      const metricHeader = metricVerboseNameMap[metric]
+        ? metricVerboseNameMap[metric]
+        : metric;
+      return {
+        field: metric,
+        headerName: metricHeader,
+        sortable: true,
+        enableRowGroup: true,
+      };
+    });
     columnDefs = columnDefs.concat(metricsColumnDefs);
   }
 
   if (percent_metrics) {
-    const percentMetricsColumnDefs = percent_metrics
-      .map((metric: any) => {
-        const metricHeader = metricVerboseNameMap[metric]
-          ? metricVerboseNameMap[metric]
-          : metric;
-        return {
-          field: `%${metric}`,
-          headerName: `%${metricHeader}`,
-          sortable: true,
-          valueFormatter: percentMetricValueFormatter,
-        };
-      });
+    const percentMetricsColumnDefs = percent_metrics.map((metric: any) => {
+      const metricHeader = metricVerboseNameMap[metric]
+        ? metricVerboseNameMap[metric]
+        : metric;
+      return {
+        field: `%${metric}`,
+        headerName: `%${metricHeader}`,
+        sortable: true,
+        valueFormatter: percentMetricValueFormatter,
+      };
+    });
     columnDefs = columnDefs.concat(percentMetricsColumnDefs);
   }
 
   return columnDefs;
-}
+};
 
-
-const calcColumnColumnDefs = (columns: QueryFormColumn[], defaultGroupBy: string[], dataset_columns: Column[], enable_row_numbers=true ) => {
-
+const calcColumnColumnDefs = (
+  columns: QueryFormColumn[],
+  defaultGroupBy: string[],
+  dataset_columns: Column[],
+  enable_row_numbers = true,
+) => {
   const columnDataMap = new Map<string, string>();
   dataset_columns.reduce(function (columnMap, column: Column) {
     // @ts-ignore
@@ -70,21 +80,20 @@ const calcColumnColumnDefs = (columns: QueryFormColumn[], defaultGroupBy: string
     // @ts-ignore
     columnMap[name] = {
       type: column.type,
-      advanced_data_type: ( (column.advanced_data_type as string) ?? ''),
+      advanced_data_type: (column.advanced_data_type as string) ?? '',
       verbose_name: column.verbose_name,
-      description: column.description
-    } 
+      description: column.description,
+    };
     return columnMap;
   }, columnDataMap);
 
-
-const columnDefs = columns.map((column: any) => { 
+  const columnDefs = columns.map((column: any) => {
     const columnType = columnDataMap[column].type;
     const advancedType = columnDataMap[column].advanced_data_type;
     const columnHeader = columnDataMap[column].verbose_name
       ? columnDataMap[column].verbose_name
       : column;
-      const cellRenderer =
+    const cellRenderer =
       advancedType.toUpperCase() in rendererMap
         ? rendererMap[advancedType.toUpperCase()]
         : columnType in rendererMap
@@ -112,30 +121,28 @@ const columnDefs = columns.map((column: any) => {
       // getQuickFilterText: (params: any) => valueFormatter(params),
       headerTooltip: columnDescription,
       autoHeight,
-    }
+    };
   });
 
   if (enable_row_numbers) {
-        columnDefs.splice(0, 0, {
-          headerName: '#',
-          colId: 'rowNum',
-          pinned: 'left',
-          width: 50,
-          lockVisible: true,
-          enableRowGroup: false,
-          valueGetter: (params: any) =>
-            params.node ? params.node.rowIndex + 1 : null,
-        } as any);
-      }
+    columnDefs.splice(0, 0, {
+      headerName: '#',
+      colId: 'rowNum',
+      pinned: 'left',
+      width: 70,
+      lockVisible: true,
+      enableRowGroup: false,
+      valueGetter: (params: any) =>
+        params.node ? params.node.rowIndex + 1 : null,
+    } as any);
+  }
 
-    return columnDefs
-}
-
-
-
+  return columnDefs;
+};
 
 export default function transformProps(chartProps: CccsTableChartProps) {
-  const {hooks, datasource, width, height, formData, queriesData } = chartProps;
+  const { hooks, datasource, width, height, formData, queriesData } =
+    chartProps;
   const {
     includeSearch,
     pageLength,
@@ -148,18 +155,31 @@ export default function transformProps(chartProps: CccsTableChartProps) {
     ...formData,
   };
 
-  
   const datasource_metrics = datasource?.metrics as Metric[];
   const data = queriesData[0].data as TimeseriesDataRecord[];
-  
-  const columns =  formData.queryMode === "raw" ? formData.allColumns || [] : formData.groupby || [];
+
+  const columns =
+    formData.queryMode === 'raw'
+      ? formData.allColumns || []
+      : formData.groupby || [];
   const metrics = formData.metrics;
   const percent_metrics = formData.percentMetrics;
 
-  let columnDefs = calcColumnColumnDefs(columns, defaultGroupBy, datasource?.columns as Column[], enableRowNumbers) 
-  columnDefs = columnDefs.concat(calcMetricColumnDefs(metrics || [], percent_metrics || [], datasource_metrics));
-  
-  const { setDataMask = () => {}, } = hooks;
+  let columnDefs = calcColumnColumnDefs(
+    columns,
+    defaultGroupBy,
+    datasource?.columns as Column[],
+    enableRowNumbers,
+  );
+  columnDefs = columnDefs.concat(
+    calcMetricColumnDefs(
+      metrics || [],
+      percent_metrics || [],
+      datasource_metrics,
+    ),
+  );
+
+  const { setDataMask = () => {} } = hooks;
 
   // If the flag is set to true, add a column which will contain
   // a button to expand all JSON blobs in the row
