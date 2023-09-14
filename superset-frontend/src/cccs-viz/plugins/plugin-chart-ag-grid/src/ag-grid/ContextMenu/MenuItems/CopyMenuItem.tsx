a5aa1ecb4b085fffb778bceca79f17f8ae655a56
cccs-RyanK <ryan.kane@cyber.gc.ca>
2023-09-14 18:04:00 +0000

    code cleanup and spelling mistakes

--- superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/ag-grid/ContextMenu/MenuItems/CopyMenuItem.tsx
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/ag-grid/ContextMenu/MenuItems/CopyMenuItem.tsx
@@ -1,41 +1,35 @@
-
-
 import { Menu } from 'src/components/Menu';
 
-import { useCallback} from 'react';
+import React, { useCallback } from 'react';
 import { CopyFilled } from '@ant-design/icons';
 
 interface CopyMenuItemProps {
-    selectedData: { [key: string]: string[] };
-    onSelection: () => void;
-    isContextMenu?: boolean;
-    contextMenuY?: number;
-  
+  selectedData: { [key: string]: string[] };
+  onSelection: () => void;
+  isContextMenu?: boolean;
+  contextMenuY?: number;
 }
 
-export default function CopyMenuItem (props: CopyMenuItemProps) {
-
-    const copyText = useCallback(
-        () => {
-            let copiedItem = "" 
-            for (const [key, value] of Object.entries(props.selectedData)) {
-                console.log(key, value);
-                copiedItem = copiedItem ? `${copiedItem},${value.toString()}`: `${value.toString()}` 
-            }
-            navigator.clipboard.writeText(copiedItem);
-            props.onSelection()
-        }, [props.selectedData]   
-    )
-    return (
-
-        <Menu.Item 
-            onItemHover={() => {}}  
-            onClick={() => copyText()}
-            key="drill-detail-no-filters"
-            className='ant-menu-item'
-            icon={<CopyFilled />}
-        >Copy</Menu.Item>
-
-    )
-
+export default function CopyMenuItem(props: CopyMenuItemProps) {
+  const copyText = useCallback(() => {
+    let copiedItem = '';
+    for (const [key, value] of Object.entries(props.selectedData)) {
+      copiedItem = copiedItem
+        ? `${copiedItem},${value.toString()}`
+        : `${value.toString()}`;
+    }
+    navigator.clipboard.writeText(copiedItem);
+    props.onSelection();
+  }, [props.selectedData]);
+  return (
+    <Menu.Item
+      onItemHover={() => {}}
+      onClick={() => copyText()}
+      key="drill-detail-no-filters"
+      className="ant-menu-item"
+      icon={<CopyFilled />}
+    >
+      Copy
+    </Menu.Item>
+  );
 }
--- superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/ag-grid/ContextMenu/MenuItems/EmitFilterMenuItem.tsx
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/ag-grid/ContextMenu/MenuItems/EmitFilterMenuItem.tsx
@@ -1,38 +1,36 @@
-
-
 import { Menu } from 'src/components/Menu';
-import { ReactNode } from 'react';
-
+import React, { ReactNode } from 'react';
 
 interface EmiteFilterMenuItemProps {
-    label: string;
-    onClick: () => void;
-    onSelection: () => void;
-    icon: ReactNode;
-    disabled?: boolean;
-    isContextMenu?: boolean;
-    contextMenuY?: number;
+  label: string;
+  onClick: () => void;
+  onSelection: () => void;
+  icon: ReactNode;
+  disabled?: boolean;
+  isContextMenu?: boolean;
+  contextMenuY?: number;
 }
 
-export default function EmiteFilterMenuItem (props: EmiteFilterMenuItemProps) {
-    const onClick = () => {
-        props.onClick()
-        props.onSelection()
-    }
+export default function EmiteFilterMenuItem(props: EmiteFilterMenuItemProps) {
+  const onClick = () => {
+    props.onClick();
+    props.onSelection();
+  };
 
-    return (
-
-        <Menu.Item 
-        onItemHover={() => {}}  
-        onClick={() => onClick()}
-        key="drill-detail-no-filters"
-        className={props.disabled ? "ant-menu-item ant-dropdown-menu-item-disabled" : 'ant-menu-item'} 
-        disabled={props.disabled}
-        icon={[props.icon]}
-        >
-          {props.label}
-        </Menu.Item>
-
-    )
-
+  return (
+    <Menu.Item
+      onItemHover={() => {}}
+      onClick={() => onClick()}
+      key="drill-detail-no-filters"
+      className={
+        props.disabled
+          ? 'ant-menu-item ant-dropdown-menu-item-disabled'
+          : 'ant-menu-item'
+      }
+      disabled={props.disabled}
+      icon={[props.icon]}
+    >
+      {props.label}
+    </Menu.Item>
+  );
 }
