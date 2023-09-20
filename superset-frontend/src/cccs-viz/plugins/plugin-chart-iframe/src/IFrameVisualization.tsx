a2ceca5d38780ec5baa70be7c8726fae7b874e6b
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2023-09-20 11:32:49 -0400

    Added edge check to IFrame Visualization, fixed error in JSON view

--- superset-frontend/src/cccs-viz/plugins/plugin-chart-iframe/src/IFrameVisualization.tsx
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-iframe/src/IFrameVisualization.tsx
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { IFrameVisualizationProps } from './types';
 
 export default function IFrameVisualization(props: IFrameVisualizationProps) {
-  const {
-    url,
-    url_parameter_value,
-    parameter_name,
-    parameter_prefix,
-    errorMessage,
-  } = props;
+  const { url, url_parameter_value, parameter_name, parameter_prefix } = props;
+
+  const [errorMessage, setErrorMessage] = React.useState<React.ReactNode>(
+    props.errorMessage,
+  );
 
   // We encode the parameter value (including its prefix)
   // so the Fission proxy doesn't break on parameters that contain, for example, URLs.
-  const encodedUrl = `${url}?${parameter_name}=${
-    parameter_prefix ? encodeURIComponent(parameter_prefix) : ''
-  }${encodeURIComponent(url_parameter_value)}`;
+  const encodedUrl = useMemo(
+    () =>
+      `${url}?${parameter_name}=${
+        parameter_prefix ? encodeURIComponent(parameter_prefix) : ''
+      }${encodeURIComponent(url_parameter_value)}`,
+    [parameter_name, parameter_prefix, url, url_parameter_value],
+  );
+
+  useEffect(() => {
+    if (
+      url.includes('fission.hogwarts') &&
+      navigator.userAgent.includes('Edg')
+    ) {
+      setErrorMessage(
+        // eslint-disable-next-line theme-colors/no-literal-colors
+        <span style={{ color: 'red', fontWeight: 'bold' }}>
+          Fission endpoints are not supported in Microsoft Edge. Open{' '}
+          <a href={encodedUrl} target="_blank" rel="noreferrer">
+            this link
+          </a>{' '}
+          to view the visualization.
+        </span>,
+      );
+    }
+  }, [encodedUrl, url]);
 
   return (
     <>
@@ -22,6 +42,7 @@ export default function IFrameVisualization(props: IFrameVisualizationProps) {
         <>{errorMessage}</>
       ) : (
         <iframe
+          title={`Rendering of ${url}`}
           src={encodedUrl}
           style={{
             position: 'absolute',
@@ -30,7 +51,7 @@ export default function IFrameVisualization(props: IFrameVisualizationProps) {
             width: '95%',
             height: '100%',
           }}
-        ></iframe>
+        />
       )}
     </>
   );
