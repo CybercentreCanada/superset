9f5522b9b2c6b26d55665e7e22181df9516e5535
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2023-09-21 12:22:18 -0400

    Reorganization, cleanup

--- superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/renderers/TimestampWithoutTimezoneValueRenderer.tsx
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/renderers/TimestampWithoutTimezoneValueRenderer.tsx
@@ -4,9 +4,8 @@ import moment from 'moment';
 
 const TimestampWithoutTimezoneValueRenderer: React.FC<{
   [index: string]: any;
-}> = React.memo(
-  // Since there's no timezone, we manually add in the UTC timezone
-  data => <>{moment(data.value).format('YYYY-MM-DD HH:mm:ss.SS UTC')}</>,
-);
+}> = React.memo(data => (
+  <>{moment(data.value).format('YYYY-MM-DD HH:mm:ss.SS')}</>
+));
 
 export default TimestampWithoutTimezoneValueRenderer;
