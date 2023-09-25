fb01881902d83e70646b5cbbf3702f8ee13efdc2
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2023-09-25 13:21:41 -0400

    Fixed iframe error handling

--- superset-frontend/src/cccs-viz/plugins/plugin-chart-iframe/src/IFrameVisualization.tsx
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-iframe/src/IFrameVisualization.tsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import { IFrameVisualizationProps } from './types';
 
 export default function IFrameVisualization(props: IFrameVisualizationProps) {
-  const { url, url_parameter_value, parameter_name, parameter_prefix } = props;
-
-  const [errorMessage, setErrorMessage] = useState<React.ReactNode>(
-    props.errorMessage,
-  );
+  const {
+    errorMessage,
+    url,
+    url_parameter_value,
+    parameter_name,
+    parameter_prefix,
+  } = props;
 
   // We encode the parameter value (including its prefix)
   // so the Fission proxy doesn't break on parameters that contain, for example, URLs.
@@ -18,39 +20,28 @@ export default function IFrameVisualization(props: IFrameVisualizationProps) {
     [parameter_name, parameter_prefix, url, url_parameter_value],
   );
 
-  useEffect(() => {
-    if (
-      url.includes('fission.hogwarts') &&
-      (navigator.userAgent.includes('Edg/') ||
-        navigator.userAgent.includes('Chrome/'))
-    ) {
-      setErrorMessage(
-        // eslint-disable-next-line theme-colors/no-literal-colors
-        <span style={{ color: 'red', fontWeight: 'bold' }}>
-          Fission endpoints are not supported in{' '}
-          {navigator.userAgent.includes('Edg/')
-            ? 'Microsoft Edge'
-            : 'Google Chrome'}
-          . Open{' '}
-          <a href={encodedUrl} target="_blank" rel="noreferrer">
-            this link
-          </a>{' '}
-          to view the visualization, or open this dashboard in Firefox.
-        </span>,
-      );
-    }
-  }, [encodedUrl, url]);
-
-  useEffect(() => {
-    if (props.errorMessage && !errorMessage) {
-      setErrorMessage(props.errorMessage);
-    }
-  }, [errorMessage, props.errorMessage]);
-
   return (
     <>
       {errorMessage ? (
-        <>{errorMessage}</>
+        errorMessage !== 'fission-incompat' ? (
+          // eslint-disable-next-line theme-colors/no-literal-colors
+          <span style={{ color: 'red', fontWeight: 'bold' }}>
+            {errorMessage}
+          </span>
+        ) : (
+          // eslint-disable-next-line theme-colors/no-literal-colors
+          <span style={{ color: 'red', fontWeight: 'bold' }}>
+            Fission endpoints are not supported in{' '}
+            {navigator.userAgent.includes('Edg/')
+              ? 'Microsoft Edge'
+              : 'Google Chrome'}
+            . Open{' '}
+            <a href={encodedUrl} target="_blank" rel="noreferrer">
+              this link
+            </a>{' '}
+            to view the visualization, or open this dashboard in Firefox.
+          </span>
+        )
       ) : (
         <iframe
           title={`Rendering of ${url}`}
