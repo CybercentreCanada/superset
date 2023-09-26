71b73ee65b429b020f512184b7f46898616ff5a8
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2023-09-26 12:33:09 -0400

    Added hardcoded formatting to some date fields

--- superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/renderers/TimestampWithoutTimezoneValueRenderer.tsx
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/renderers/TimestampWithoutTimezoneValueRenderer.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { memo } from 'react';
 
 import moment from 'moment-timezone';
 
+/**
+ * Defines a list of hardcoded formats when the column definition has a matching field
+ * name - i.e. "year" will always show the year only, instead of the full date
+ */
+const HARDCODED_FORMATS = {
+  year: 'Y',
+  month: 'MMMM',
+  week: 'W',
+};
+
 const TimestampValueRenderer: React.FC<{
   [index: string]: any;
-}> = React.memo(data => {
-  const timezone = moment(data.value).format('zz');
+}> = memo(data => {
+  const date = moment(data.value);
+
+  const timezone = date.format('zz');
 
   return (
     <>
-      {moment(data.value).format('YYYY-MM-DD HH:mm:ss.SS') +
-        (timezone ? `(${timezone})` : '')}
+      {date.format(
+        HARDCODED_FORMATS[data.colDef?.field?.toLowerCase()] ??
+          'YYYY-MM-DD HH:mm:ss.SS',
+      ) + (timezone ? `(${timezone})` : '')}
     </>
   );
 });
