a7d974ab4b8b2b12eed1544774d414995bacc755
cccs-RyanK <102618419+cccs-RyanK@users.noreply.github.com>
2024-01-02 13:55:36 -0500

    Update utils.py

--- superset/fission/utils.py
+++ superset/fission/utils.py
@@ -255,7 +255,7 @@ def retain_eml_to_alfred(ids, alfred_env, access_token, dates=None):
             logger.info(f'Found dates: "{ids_string}" in arguments')
             for date in dates:
                 try:
-                    datetimes.append(datetime.strptime(date, "%d-%m-%Y"))
+                    datetimes.append(datetime.strptime(date, "%m-%d-%Y"))
                 except ValueError as ve:
                     logger.info(ve)
                     continue
