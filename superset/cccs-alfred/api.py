37e07aef3bb7b66c6c844b5696ffd8e0b2e279c3
cccs-RyanK <ryan.kane@cyber.gc.ca>
2024-01-05 16:22:41 +0000

    moving to new api directory

--- superset/cccs-alfred/api.py
+++ superset/cccs-alfred/api.py
@@ -37,58 +37,59 @@ from superset.fission.utils import retain_eml_to_alfred
 logger = logging.getLogger(__name__)
 
 config = app.config
-API_HOST = os.environ.get("FISSION_PROXY_URL")
 
-
-class FissionRestApi(BaseApi):
+class AlfredRestApi(BaseApi):
     """
     Fission rest endpoint to proxy hogwarts fission
     """
 
     allow_browser_login = True
-    include_route_methods = {RouteMethod.GET, RouteMethod.POST}
-    resource_name = "fission"
+    include_route_methods = {RouteMethod.POST}
+    resource_name = "cccs-alfred"
     class_permission_name = "AdvancedDataType"
 
-    openapi_spec_tag = "Fission"
+    openapi_spec_tag = "Alfred"
     openapi_spec_component_schemas = (AdvancedDataTypeSchema,)
 
     @protect()
     @safe
-    @expose("/<path>", methods=["GET"])
+    @expose("/retain-eml-record", methods=["POST"])
     @permission_name("read")
     @event_logger.log_this_with_context(
-        action=lambda self, *args, **kwargs: f"{self.__class__.__name__}.get",
+        action=lambda self, *args, **kwargs: f"{self.__class__.__name__}.post",
         log_to_statsd=False,  # pylint: disable-arguments-renamed
     )
-    def get(self, path, **kwargs: Any) -> Response:
-        """Proxy to hogwarts fission"""
+    def post(self, **kwargs: Any) -> Response:
+        """
+        Fetch the email metadata associated with the email IDs sent in the 
+        request and send the data to alfred to create a staged retention
+
+        Returns:
+            Response: 
+                200 Success, { result: "<alfred-url-for-staged-retention>"}
+        """
+        request_payload = request.json
+        if not request_payload:
+            logger.info("no JSON payload in request")
+            return self.response_400('no JSON payload in request')
+        if 'email_ids' not in request_payload:
+            return self.response_400('No field email_ids found in json body of request')
+        alfred_env = os.environ.get("ALFRED_ENV")
+        if not alfred_env:
+            logger.info("ALFRED_ENV environment variable not set")
+            return self.response_400('ALFRED_ENV environment variable not set')        
+        logger.info("Payload is %s", request_payload)
+        email_ids = request_payload['email_ids']
+        dates = None
+        if 'dates' in request_payload:
+            dates = request_payload['dates']
         user = current_user
         token = security_manager.get_on_behalf_of_access_token_with_cache(
             user.username,
-            os.environ.get("FISSION_SCOPE"),
+            os.environ.get("SUPERSET_SCOPE"),
             "superset",
             cache_result=True,
         )
-
-        logger.info("Args is %s", request.args)
-        headers = {
-            "Authorization": f"Bearer {token}",
-            "X-Auth-Request-Access-Token": token,
-        }
-
-        url = request.url.replace(f"{request.host_url}api/v1/fission/", f"{API_HOST}/")
-        res = requests.request(  # ref. https://stackoverflow.com/a/36601467/248616
-            method=request.method,
-            url=url,
-            data=request.get_data(),
-            allow_redirects=False,
-            headers=headers,
-            timeout=180,
-        )
-        try:
-            result = res.json()
-        except requests.JSONDecodeError as e:
-            logger.error('response does not contain valid json: %s', e)
-            result = str(res.text)
-        return self.response(res.status_code, result=result)
+        status, result = retain_eml_to_alfred(email_ids, alfred_env, token, dates)
+        logger.info('result: %s', result)
+        return self.response(status, result=result)
