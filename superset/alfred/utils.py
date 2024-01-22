fe015f15a193d8fb74256d637758bf0bded0f9a3
cccs-RyanK <102618419+cccs-RyanK@users.noreply.github.com>
2024-01-22 09:20:07 -0500

    Update utils.py

--- superset/alfred/utils.py
+++ superset/alfred/utils.py
@@ -276,7 +276,7 @@ def retain_eml_to_alfred(ids, alfred_env, access_token, dates=None):
         )
 
         sql = f"""
-    SELECT id, urls, "from", to, message_id, reply_to, stream_id, subject, eml_path, bcc, cc, ministerial_authorization, classification, time
+    SELECT *
     FROM  hogwarts_pb.harmonized.eml_metadata
     WHERE id IN ('{ids_string}')
     """
