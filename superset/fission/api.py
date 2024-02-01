64de53de333b39a998538c5447c86363e6b7ea74
cccs-RyanK <102618419+cccs-RyanK@users.noreply.github.com>
2024-02-01 15:43:36 -0500

    Retain to alfred with post (#384)
    * changing alfred to use post instead of get and include dates
    
    * Update Dockerfile
    
    * small fixes to retain to alfred
    
    * added content type header
    
    * update base image
    
    * bump timeout up
    
    * fix var name
    
    * better check for bad date
    
    * taking out fission
    
    * Update analytical-platform-requirements.txt
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update Dockerfile
    
    * Update requirements.txt
    
    * Update analytical-platform-requirements.txt
    
    * remove path argument
    
    * Update Dockerfile
    
    * Update utils.py
    
    * Update Dockerfile
    
    * Update utils.py
    
    * Update Dockerfile
    
    * Update api.py
    
    * Update Dockerfile
    
    * Update api.py
    
    * Update utils.py
    
    * Update Dockerfile
    
    * Update api.py
    
    * Update Dockerfile
    
    * moving to new api directory
    
    * refactoring
    
    * fixing schema
    
    * update dockerfile image
    
    * Update api.py
    
    * Update superset/alfred/api.py
    
    Co-authored-by: cccs-rc <62034438+cccs-rc@users.noreply.github.com>
    
    * Update superset/alfred/api.py
    
    Co-authored-by: cccs-rc <62034438+cccs-rc@users.noreply.github.com>
    
    * fixing logging statements where necessary
    
    * bumping limit to 100
    
    * removing cccs_formatting arg that is always true
    
    * Update superset/alfred/utils.py
    
    Co-authored-by: cccs-rc <62034438+cccs-rc@users.noreply.github.com>
    
    * Update superset/alfred/utils.py
    
    Co-authored-by: cccs-rc <62034438+cccs-rc@users.noreply.github.com>
    
    * Update superset/alfred/utils.py
    
    Co-authored-by: cccs-rc <62034438+cccs-rc@users.noreply.github.com>
    
    * Update superset/alfred/utils.py
    
    Co-authored-by: cccs-rc <62034438+cccs-rc@users.noreply.github.com>
    
    * cleaning up utils
    
    * trino host env var
    
    * Update superset/alfred/api.py
    
    Co-authored-by: cccs-joel <62111795+cccs-joel@users.noreply.github.com>
    
    * Update superset/alfred/api.py
    
    Co-authored-by: cccs-joel <62111795+cccs-joel@users.noreply.github.com>
    
    * Update superset/alfred/utils.py
    
    Co-authored-by: cccs-joel <62111795+cccs-joel@users.noreply.github.com>
    
    * Update superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/ag-grid/AGGridViz.tsx
    
    Co-authored-by: cccs-joel <62111795+cccs-joel@users.noreply.github.com>
    
    * remove unused variable
    
    * limit is 100
    
    * moved to constant
    
    * syntax
    
    * Update superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/ag-grid/AGGridViz.tsx
    
    Co-authored-by: cccs-joel <62111795+cccs-joel@users.noreply.github.com>
    
    * Update Dockerfile
    
    * fixing date times
    
    * adding more broad date column names and catching on errors parsing dates
    
    * fixed date formatting
    
    * Update Dockerfile
    
    * fixed errors
    
    * update dockerfile
    
    * fixed list format
    
    * update dockerfile
    
    * fixed flattening array of arrays
    
    * update docker
    
    * count unique eml ids when retaining
    
    * Update Dockerfile
    
    * added extra brackets and fixed error message
    
    * update docker file
    
    * Update utils.py
    
    * adding microsecond precision to json conversion
    
    * updated dockerfile
    
    * double quotes
    
    * Update Dockerfile
    
    * fix query
    
    * added functionality to santize function
    
    * Update Dockerfile
    
    * testing async retention
    
    * update docker
    
    * fixed small issue
    
    * update dockerfile
    
    * added comments
    
    * Update utils.py
    
    ---------
    
    Co-authored-by: cccs-rc <62034438+cccs-rc@users.noreply.github.com>
    Co-authored-by: cccs-joel <62111795+cccs-joel@users.noreply.github.com>

--- superset/fission/api.py
+++ superset/fission/api.py
@@ -14,39 +14,36 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import base64
-from typing import Any
 import logging
-from flask import current_app as app
+import os
+from typing import Any
+
+import requests  # pip package requests
+from flask import current_app as app, request, Response
 from flask.wrappers import Response
-from flask_appbuilder.api import BaseApi, expose, permission_name, protect, rison, safe
+from flask_appbuilder.api import BaseApi, expose, permission_name, protect, safe
 from flask_babel import lazy_gettext as _
 from flask_login import current_user
-from superset.extensions import (
-    security_manager
-)
+
 from superset.advanced_data_type.schemas import (
-    advanced_data_type_convert_schema,
     AdvancedDataTypeSchema,
 )
-from superset.advanced_data_type.types import AdvancedDataTypeResponse
-from superset.extensions import event_logger
-
-from flask import request, Response, current_app
-import requests  # pip package requests
-import os
+from superset.constants import RouteMethod
+from superset.extensions import event_logger, security_manager
 
 logger = logging.getLogger(__name__)
 
 config = app.config
-API_HOST=os.environ.get('FISSION_PROXY_URL')
+API_HOST = os.environ.get("FISSION_PROXY_URL")
+
 
 class FissionRestApi(BaseApi):
     """
-      Fission rest endpoint to proxy hogwarts fission
+    Fission rest endpoint to proxy hogwarts fission
     """
+
     allow_browser_login = True
-    include_route_methods = {"get"}
+    include_route_methods = {RouteMethod.GET, RouteMethod.POST}
     resource_name = "fission"
     class_permission_name = "AdvancedDataType"
 
@@ -62,23 +59,23 @@ class FissionRestApi(BaseApi):
         log_to_statsd=False,  # pylint: disable-arguments-renamed
     )
     def get(self, path, **kwargs: Any) -> Response:
-        """Proxy to hogwarts fission
-        """
+        """Proxy to hogwarts fission"""
         user = current_user
-        token = security_manager.get_on_behalf_of_access_token_with_cache(user.username,
-                                                                          os.environ.get('FISSION_SCOPE'),
-                                                                          'superset',
-                                                                          cache_result=True)
-        
-        logger.info('Args is %s', request.args)
+        token = security_manager.get_on_behalf_of_access_token_with_cache(
+            user.username,
+            os.environ.get("FISSION_SCOPE"),
+            "superset",
+            cache_result=True,
+        )
+
+        logger.info("Args is %s", request.args)
         headers = {
-          'Authorization': f"Bearer {token}",
-          "X-Auth-Request-Access-Token": token
+            "Authorization": f"Bearer {token}",
+            "X-Auth-Request-Access-Token": token,
         }
 
-        url = request.url.replace(f'{request.host_url}api/v1/fission', f'{API_HOST}/')
-
-        res = requests.request( # ref. https://stackoverflow.com/a/36601467/248616
+        url = request.url.replace(f"{request.host_url}api/v1/fission/", f"{API_HOST}/")
+        res = requests.request(  # ref. https://stackoverflow.com/a/36601467/248616
             method          = request.method,
             url             = url,
             data            = request.get_data(),
@@ -86,14 +83,9 @@ class FissionRestApi(BaseApi):
             headers         = headers,
             timeout         = 180
         )
-
-        if res.headers.get('Content-Type') == 'image/png':  # Check if the response is an image
-            encoded_image = base64.b64encode(res.content).decode('utf-8')  # Encode the image in base64
-            result = {'image': f'data:image/png;base64,{encoded_image}'}  # Store in a JSON-friendly format
-        else:
-            try:
-                result = res.json()
-            except:
-                result = str(res.text)
-
+        try:
+            result = res.json()
+        except requests.JSONDecodeError as e:
+            logger.error('response does not contain valid json: %s', e)
+            result = str(res.text)
         return self.response(res.status_code, result=result)
