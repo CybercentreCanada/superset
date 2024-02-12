4c67bb5abadd12fff3a7cebf83273e113eb01d35
nz970 <yuying.zhang@cyber.gc.ca>
2024-02-12 20:21:16 +0000

    Edited request call in fission api

--- superset/fission/api.py
+++ superset/fission/api.py
@@ -14,39 +14,36 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import base64
-from typing import Any
 import logging
-from flask import current_app as app
+import os
+from typing import Any
+
+import requests  # pip package requests
+from flask import current_app as app, request, Response
 from flask.wrappers import Response
-from flask_appbuilder.api import BaseApi, expose, permission_name, protect, rison, safe
+from flask_appbuilder.api import BaseApi, expose, permission_name, protect, safe
 from flask_babel import lazy_gettext as _
 from flask_login import current_user
-from superset.extensions import (
-    security_manager
-)
+
 from superset.advanced_data_type.schemas import (
-    advanced_data_type_convert_schema,
     AdvancedDataTypeSchema,
 )
-from superset.advanced_data_type.types import AdvancedDataTypeResponse
-from superset.extensions import event_logger
-
-from flask import request, Response, current_app
-import requests  # pip package requests
-import os
+from superset.constants import RouteMethod
+from superset.extensions import event_logger, security_manager
 
 logger = logging.getLogger(__name__)
 
 config = app.config
-API_HOST=os.environ.get('FISSION_PROXY_URL')
+API_HOST = os.environ.get("FISSION_PROXY_URL")
+
 
 class FissionRestApi(BaseApi):
     """
-      Fission rest endpoint to proxy hogwarts fission
+    Fission rest endpoint to proxy hogwarts fission
     """
+
     allow_browser_login = True
-    include_route_methods = {"get"}
+    include_route_methods = {RouteMethod.GET, RouteMethod.POST}
     resource_name = "fission"
     class_permission_name = "AdvancedDataType"
 
@@ -62,38 +59,43 @@ class FissionRestApi(BaseApi):
         log_to_statsd=False,  # pylint: disable-arguments-renamed
     )
     def get(self, path, **kwargs: Any) -> Response:
-        """Proxy to hogwarts fission
-        """
+        """Proxy to hogwarts fission"""
         user = current_user
-        token = security_manager.get_on_behalf_of_access_token_with_cache(user.username,
-                                                                          os.environ.get('FISSION_SCOPE'),
-                                                                          'superset',
-                                                                          cache_result=True)
-        
-        logger.info('Args is %s', request.args)
+        token = security_manager.get_on_behalf_of_access_token_with_cache(
+            user.username,
+            os.environ.get("FISSION_SCOPE"),
+            "superset",
+            cache_result=True,
+        )
+
+        logger.info("Args is %s", request.args)
         headers = {
-          'Authorization': f"Bearer {token}",
-          "X-Auth-Request-Access-Token": token
+            "Authorization": f"Bearer {token}",
+            "X-Auth-Request-Access-Token": token,
         }
 
-        url = request.url.replace(f'{request.host_url}api/v1/fission', f'{API_HOST}/')
+        path_to_replace = ""
+        if "api/v1/me" in request.host_url:
+            path_to_replace = "api/v1/me/"
+        elif "api/v1/fission" in request.host_url:
+            path_to_replace = "api/v1/fission/"
 
-        res = requests.request( # ref. https://stackoverflow.com/a/36601467/248616
+        # Check if a path was set for replacement
+        if path_to_replace:
+            # Replace the relevant part of the URL with the API_HOST
+            url = request.url.replace(f"{request.host_url}{path_to_replace}", f"{API_HOST}/")
+
+        res = requests.request(  # ref. https://stackoverflow.com/a/36601467/248616
             method          = request.method,
             url             = url,
             data            = request.get_data(),
             allow_redirects = False,
             headers         = headers,
-            timeout         = 30
+            timeout         = 180 # extending timeout for fission loading times
         )
-
-        if res.headers.get('Content-Type') == 'image/png':  # Check if the response is an image
-            encoded_image = base64.b64encode(res.content).decode('utf-8')  # Encode the image in base64
-            result = {'image': f'data:image/png;base64,{encoded_image}'}  # Store in a JSON-friendly format
-        else:
-            try:
-                result = res.json()
-            except:
-                result = str(res.text)
-
+        try:
+            result = res.json()
+        except requests.JSONDecodeError as e:
+            logger.error('response does not contain valid json: %s', e)
+            result = str(res.text)
         return self.response(res.status_code, result=result)
