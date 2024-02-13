9deae3c6646609cffe290661e33975615ceebb03
nz970 <yuying.zhang@cyber.gc.ca>
2024-02-13 18:59:39 +0000

    request call edited

--- superset/fission/api.py
+++ superset/fission/api.py
@@ -75,7 +75,7 @@ class FissionRestApi(BaseApi):
             "X-Auth-Request-Access-Token": token,
         }
 
-        url = request.url.replace(f"{request.url}api/v1/fission/", f"{API_HOST}/") 
+        url = request.url.replace(f"{request.host_url}api/v1/fission/", f"{API_HOST}/") 
 
         res = requests.request(  # ref. https://stackoverflow.com/a/36601467/248616
             method          = request.method,
