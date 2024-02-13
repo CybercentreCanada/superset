0223f2d3d98da46795b1e15178a88772ba5a66b0
nz970 <yuying.zhang@cyber.gc.ca>
2024-02-13 21:10:32 +0000

    removed url param

--- superset-frontend/src/cccs-viz/plugins/plugin-chart-email-renderer/src/types.ts
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-email-renderer/src/types.ts
@@ -3,7 +3,6 @@ import { QueryFormData } from '@superset-ui/core';
 export type EmailRendererProps = QueryFormData & {
   url_parameter_value: string;
   parameter_name: string;
-  url: string;
   parameter_prefix: string;
   errorMessage: string;
 };
