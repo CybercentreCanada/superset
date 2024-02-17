f74462ad87d7049b71dd59ac278d3d17601f0cc3
jansdhillon <jandhill@student.ubc.ca>
2024-02-17 00:19:04 +0000

    init tests for main superset deployment

--- tests/unit_tests/advanced_data_type/types_tests.py
+++ tests/unit_tests/advanced_data_type/types_tests.py
@@ -513,3 +513,85 @@ def test_port_translate_filter_func_not_in_double():
     assert port.translate_filter(input_column, input_operation, input_values).compare(
         port_translate_filter_response
     )
+
+from superset.advanced_data_type import STREAM_DICT, stream_id_func, ipv6_func, domain_func
+import unittest
+
+class TestTypes(unittest.TestCase):
+
+    """
+    Test for advanced data types
+    """
+    def test_stream_id(self):
+        print("Testing stream ID...\n")
+        self.known_key = 11
+        self.known_key_2 = 129
+        self.known_value = "IIS_EXT"
+        self.known_value_2 = "MT-ASSEMBLYLINE-U-PROD"
+        self.assertEqual(list(STREAM_DICT.keys())[list(STREAM_DICT.values()).index(self.known_value)], self.known_key)
+        self.assertEqual(list(STREAM_DICT.keys())[list(STREAM_DICT.values()).index(self.known_value_2)], self.known_key_2)
+        self.assertNotEqual(list(STREAM_DICT.keys())[list(STREAM_DICT.values()).index(self.known_value)], self.known_key_2)
+        self.assertNotEqual(list(STREAM_DICT.keys())[list(STREAM_DICT.values()).index(self.known_value)], self.known_key_2)
+        val_req: AdvancedDataTypeRequest = {
+        "values": ["IIS_EXT"]
+        }
+        key_req: AdvancedDataTypeRequest = {
+        "values": [11]
+        }
+        test_key:AdvancedDataTypeResponse = stream_id_func(key_req)
+        test_val:AdvancedDataTypeResponse = stream_id_func(val_req)
+        self.assertEqual(test_key["error_message"], "")
+        self.assertEqual(test_val["error_message"], "")
+        invalid_val_req: AdvancedDataTypeRequest = {
+        "values": ["fail"]
+        }
+        invalid_key_req: AdvancedDataTypeRequest = {
+        "values": [0]
+        }
+        test_invalid_key:AdvancedDataTypeResponse = stream_id_func(invalid_key_req)
+        test_invalid_val:AdvancedDataTypeResponse = stream_id_func(invalid_val_req)
+        self.assertEqual(test_invalid_val['error_message'], f"'{invalid_val_req['values'][0]}' is not a valid Stream ID. Did not match a known Stream ID or name.")
+        self.assertEqual(test_invalid_key['error_message'], f"'{invalid_key_req['values'][0]}' is not a valid Stream ID. Did not match a known Stream ID or name.")
+        print("Stream ID passed!\n")
+
+    def test_ipv6_adddress(self):
+        print("Testing IPv6 Address...\n")
+        valid_ipv6_req: AdvancedDataTypeRequest = {
+            "values": ["2001:0db8:85a3:0000:0000:8a2e:0370:7334"],
+            "error_message": "",
+            "display_value": "",
+        }
+        test_ipv6:AdvancedDataTypeResponse = ipv6_func(valid_ipv6_req)
+        self.assertEqual(test_ipv6["error_message"], "")
+        invalid_ipv6_req: AdvancedDataTypeRequest = {
+            "values": [11111111],
+            "error_message": "",
+            "display_value": "",
+        }
+        test_invalid_ipv6:AdvancedDataTypeResponse = ipv6_func(invalid_ipv6_req)
+        self.assertEqual(test_invalid_ipv6['error_message'], f"'{invalid_ipv6_req['values'][0]}' is not a valid IPv6 address.")
+        print("IPv6 passed!\n")
+
+    def test_domain(self):
+        print("Testing domain...\n")
+        valid_domain_req: AdvancedDataTypeRequest = {
+            "values": ["https://helloworld.com"],
+            "operator": "EQUALS",
+            "error_message": "",
+            "display_value": "",
+        }
+        test_domain:AdvancedDataTypeResponse = domain_func(valid_domain_req)
+        self.assertEqual(test_domain["error_message"], "")
+        inavlid_domain_req: AdvancedDataTypeRequest = {
+            "values": [""],
+            "error_message": "",
+            "display_value": "",
+            "operator": "EQUALS",
+        }
+        test_invalid_domain:AdvancedDataTypeResponse = domain_func(inavlid_domain_req)
+        self.assertEqual(test_invalid_domain['error_message'], f"Domain must not be empty")
+        print("Domain passed!\n")
+
+
+if __name__ == '__main__':
+    unittest.main()
