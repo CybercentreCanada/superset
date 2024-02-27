ab79dfb66d12af57f59bada047400d994663377b
nz970 <yuying.zhang@cyber.gc.ca>
2024-02-27 22:25:25 +0000

    PR changes completed

--- superset/charts/data/api.py
+++ superset/charts/data/api.py
@@ -361,7 +361,6 @@ class ChartDataRestApi(ChartRestApi):
             result["queries"][0]["agGridLicenseKey"] = config["AG_GRID_LICENSE_KEY"]
             result["queries"][0]["assemblyLineUrl"] = config["ASSEMBLY_LINE_URL"]
             result["queries"][0]["enableAlfred"] = config["ENABLE_ALFRED"]
-
         
         result["queries"][0]["fissionUrl"] = config["FISSION_PROXY_URL"]
             
