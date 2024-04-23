879d7d12bfc7394bef1ae5028f0f9c5cfdfc2591
nz970 <yuying.zhang@cyber.gc.ca>
2024-04-23 19:58:27 +0000

    Changed icon to Alsvg

--- superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/ag-grid/ContextMenu/MenuItems/SubmitToAssemblyLineMenuItem.tsx
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/ag-grid/ContextMenu/MenuItems/SubmitToAssemblyLineMenuItem.tsx
@@ -7,60 +7,64 @@ import {
   addDangerToast,
 } from 'src/components/MessageToasts/actions';
 import Icon from '@ant-design/icons';
-import EmailSvg from '../../../cccs-grid/images/email.svg';
+import AlSvg from '../../../cccs-grid/images/al.svg';
 
 interface SubmitToAssemblyLineMenuItemProps {
-    label: string;
-    data: string;
-    onSelection: () => void;
-    key?: string;
-    disabled?: boolean;
-    isContextMenu?: boolean;
-    contextMenuY?: number;
-  }
+  label: string;
+  data: string;
+  onSelection: () => void;
+  key?: string;
+  disabled?: boolean;
+  isContextMenu?: boolean;
+  contextMenuY?: number;
+}
 
-  export default function SubmitToAssemblyLineMenuItem(props: SubmitToAssemblyLineMenuItemProps) {
-    const dispatch = useDispatch();
+export default function SubmitToAssemblyLineMenuItem(
+  props: SubmitToAssemblyLineMenuItemProps,
+) {
+  const dispatch = useDispatch();
 
-    const fetchDataAndDisplayJson = async () => {
-      const endpoint = `/api/v1/fission/submitassemblyline?file=${props.data}`;
-      try {
-        dispatch(addInfoToast('Submission started'));
-        const response = await SupersetClient.get({ endpoint, timeout: 180000 }); // 3 minutes
-        const json = response.json;
+  const fetchDataAndDisplayJson = async () => {
+    const endpoint = `/api/v1/fission/submitassemblyline?file=${props.data}`;
+    try {
+      dispatch(addInfoToast('Submission started'));
+      const response = await SupersetClient.get({ endpoint, timeout: 180000 }); // 3 minutes
+      const json = response.json;
 
-        // Ensure there's JSON data to display
-        if (json) {
-          const jsonBlob = new Blob([JSON.stringify(json, null, 2)], { type: 'application/json' });
-          const jsonDataUrl = window.URL.createObjectURL(jsonBlob);
-          window.open(jsonDataUrl, '_blank');
-        } else {
-          dispatch(addDangerToast('No content to display.'));
-        }
-      } catch (error) {
-        // Log the error or send it to a monitoring service
-        console.error('Failed to submit to AssemblyLine due to: ', error);
-        dispatch(addDangerToast('Failed to submit to AssemblyLine.'));
+      // Ensure there's JSON data to display
+      if (json) {
+        const jsonBlob = new Blob([JSON.stringify(json, null, 2)], {
+          type: 'application/json',
+        });
+        const jsonDataUrl = window.URL.createObjectURL(jsonBlob);
+        window.open(jsonDataUrl, '_blank');
+      } else {
+        dispatch(addDangerToast('No content to display.'));
       }
-    };
+    } catch (error) {
+      // Log the error or send it to a monitoring service
+      console.error('Failed to submit to AssemblyLine due to: ', error);
+      dispatch(addDangerToast('Failed to submit to AssemblyLine.'));
+    }
+  };
 
-    const onClick = () => {
-      fetchDataAndDisplayJson();
-      props.onSelection();
-    };
+  const onClick = () => {
+    fetchDataAndDisplayJson();
+    props.onSelection();
+  };
 
-    return (
-      <Menu.Item
-        onClick={onClick}
-        className={
-          props.disabled
-            ? 'ant-dropdown-menu-item ant-dropdown-menu-item-disabled'
-            : 'ant-dropdown-menu-item'
-        }
-        disabled={props.disabled}
-        icon={<Icon component={EmailSvg} />}
-      >
-        {props.label}
-      </Menu.Item>
-    );
-  }
+  return (
+    <Menu.Item
+      onClick={onClick}
+      className={
+        props.disabled
+          ? 'ant-dropdown-menu-item ant-dropdown-menu-item-disabled'
+          : 'ant-dropdown-menu-item'
+      }
+      disabled={props.disabled}
+      icon={<Icon component={AlSvg} />}
+    >
+      {props.label}
+    </Menu.Item>
+  );
+}
