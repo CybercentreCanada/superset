aecf6bb3fb6ac092ffa55b33716078436ce5f2a2
nz970 <yuying.zhang@cyber.gc.ca>
2024-04-30 17:17:29 +0000

    Removed package.json files

---
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/ag-grid/ContextMenu/MenuItems/SubmitToAssemblyLineMenuItem.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Menu } from 'src/components/Menu';
+import Icon from '@ant-design/icons';
+import AlSvg from '../../../cccs-grid/images/al.svg';
+
+interface SubmitToAssemblyLineMenuItemProps {
+  label: string;
+  data: string;
+  onSelection: () => void;
+  key?: string;
+  disabled?: boolean;
+  isContextMenu?: boolean;
+  contextMenuY?: number;
+}
+
+export default function SubmitToAssemblyLineMenuItem(
+  props: SubmitToAssemblyLineMenuItemProps,
+) {
+  const onClick = () => {
+    const url = `https://malware-stg.cyber.gc.ca/submit?input=${props.data}&classification=TLP:CLEAR`;
+    window.open(url);
+    props.onSelection();
+  };
+
+  return (
+    <Menu.Item
+      {...props}
+      onClick={onClick}
+      className={
+        props.disabled
+          ? 'ant-dropdown-menu-item ant-dropdown-menu-item-disabled'
+          : 'ant-dropdown-menu-item'
+      }
+      disabled={props.disabled}
+      icon={<Icon component={AlSvg} />}
+    >
+      {props.label}
+    </Menu.Item>
+  );
+}
