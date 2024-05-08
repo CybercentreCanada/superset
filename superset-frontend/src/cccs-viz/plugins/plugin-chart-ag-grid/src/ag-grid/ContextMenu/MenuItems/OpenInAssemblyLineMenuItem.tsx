b43b0aba70cfe5799bd8ea4a7f74a1ad9d0d0d22
cccs-RyanK <ryan.kane@cyber.gc.ca>
2024-05-08 09:32:07 -0400

    swap al logo to png

--- superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/ag-grid/ContextMenu/MenuItems/OpenInAssemblyLineMenuItem.tsx
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/ag-grid/ContextMenu/MenuItems/OpenInAssemblyLineMenuItem.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Menu } from 'src/components/Menu';
-import Icon from '@ant-design/icons';
-import AlSvg from '../../../cccs-grid/images/al.svg';
+import AssemblyLineIcon from 'src/cccs-viz/plugins/components/AssemblyLineIcon';
 
 interface OpenInAssemblyLineMenuItemProps {
   label: string;
@@ -34,7 +33,7 @@ export default function OpenInAssemblyLineMenuItem(
           : 'ant-dropdown-menu-item'
       }
       disabled={props.disabled}
-      icon={<Icon component={AlSvg} />}
+      icon={<AssemblyLineIcon />}
     >
       {props.label}
     </Menu.Item>
