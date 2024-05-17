9beef466826feee27151f2974f223808b48003c0
nz970 <yuying.zhang@cyber.gc.ca>
2024-05-17 14:17:28 +0000

    Prettier errors edited

--- superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/ag-grid/AGGridViz.tsx
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/ag-grid/AGGridViz.tsx
@@ -355,9 +355,7 @@ export default function AGGridViz({
               disabled={!Object.keys(selectedData.highlightedData).length}
             />
           }
-          tooltip={
-            `Cross-filter(s) will be applied to all of the charts whose datasets contain columns with the same name.`
-          }
+          tooltip={`Cross-filter(s) will be applied to all of the charts whose datasets contain columns with the same name.`}
         />,
         <EmitFilterMenuItem
           onClick={() => {
