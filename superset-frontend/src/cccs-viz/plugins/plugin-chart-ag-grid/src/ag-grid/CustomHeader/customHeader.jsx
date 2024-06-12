52ad4ae213e7b014a2f28c308b57a53bb6b99d69
nz970 <yuying.zhang@cyber.gc.ca>
2024-06-12 21:56:41 +0000

    Header errors corrected

--- superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/ag-grid/CustomHeader/customHeader.jsx
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-ag-grid/src/ag-grid/CustomHeader/customHeader.jsx
@@ -1,70 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
 
 export default props => {
-  const [ascSort, setAscSort] = useState('inactive');
-  const [descSort, setDescSort] = useState('inactive');
-  const [noSort, setNoSort] = useState('inactive');
   const refButton = useRef(null);
   const refLabel = useRef(null);
 
   const onMenuClicked = () => {
     props.showColumnMenu(refButton.current);
   };
-
-  const onSortChanged = () => {
-    setAscSort(props.column.isSortAscending() ? 'active' : 'inactive');
-    setDescSort(props.column.isSortDescending() ? 'active' : 'inactive');
-    setNoSort(
-      !props.column.isSortAscending() && !props.column.isSortDescending()
-        ? 'active'
-        : 'inactive',
-    );
-  };
-
-  const onSortRequested = (order, event) => {
-    props.setSort(order, event.shiftKey);
-  };
-
-  useEffect(() => {
-    props.column.addEventListener('sortChanged', onSortChanged);
-    onSortChanged();
-  }, []);
-
-  let sort = null;
-
-  if (props.enableSorting) {
-    sort = (
-      <>
-        <div
-          onClick={event => onSortRequested('asc', event)}
-          onTouchEnd={event => onSortRequested('asc', event)}
-          className={`customSortDownLabel ${ascSort}`}
-          role="button"
-          tabIndex="0"
-        >
-          <i className="fa fa-long-arrow-alt-down" />
-        </div>
-        <div
-          onClick={event => onSortRequested('desc', event)}
-          onTouchEnd={event => onSortRequested('desc', event)}
-          className={`customSortUpLabel ${descSort}`}
-          role="button"
-          tabIndex="0"
-        >
-          <i className="fa fa-long-arrow-alt-up" />
-        </div>
-        <div
-          onClick={event => onSortRequested('', event)}
-          onTouchEnd={event => onSortRequested('', event)}
-          className={`customSortRemoveLabel ${noSort}`}
-          role="button"
-          tabIndex="0"
-        >
-          <i className="fa fa-times" />
-        </div>
-      </>
-    );
-  }
 
   return (
     <div className="headerWrapper">
@@ -75,12 +17,11 @@ export default props => {
         role="button"
         tabIndex="0"
       >
-        <i className={`fa ${props.menuIcon}`} />
+        <i className="fa fa-bars" />
       </div>
       <div ref={refLabel} className="customHeaderLabel">
         {props.displayName}
       </div>
-      {sort}
     </div>
   );
 };
